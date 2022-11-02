22312cea0441763d65124b22cfdf60608bbc2ff8
SalahEddine3343 <salah.bouaza@cuillizi.dz>
2022-11-02 19:35:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# couch_surfing
+ 
+--data in the document
+
+-ABout:
+userID (the one in firebase)
+first name #1
+last name #1
+M/F #1 
+Hosting Availability  accepting/maybe/not accepting guests
+where i live now
+Where I Grew Up
+Occupation
+Education
+Languages I'm Fluent In #1
+Languages I'm Learning
+About Me 
+Why I'm on Couchsurfing
+My Interests    (array of interests)
+My Favorite Music, Movies & Books
+One Amazing Thing I've Done
+Teach, Learn, Share
+What I Can Share With Hosts
+Countries I've Visited
+Countries I've Lived In
+
+-my home:
+Available Nights to Host (check days)
+Maximum Number of Guests
+Preferred Gender to Host
+Children, Pets, Smoking (check boxes (are they allowed))
+Sleeping Arrangements (shared/private/public room)
+Description of Sleeping Arrangement
+Roommate Situation
+Miscellaneous (me (pets/have kids/smoking))
+Public Transportation Access (yes/no) (actually a text input)
+What I Can Share With Guests
+Additional Information
+food included?
---
+++ database/user-class.js
@@ -0,0 +1,46 @@
+class user {
+    constructor(name, year) {
+        this.userid = name;
+        this.firstname = year;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.accepting_guests = accepting_guests;
+        this.wilaya = wilaya;
+        this.daira = daira;
+        this.occupation = occupation;
+        this.education = education;
+        this.aboutme = aboutme;
+        this.why_im_hosting = why_im_hosting;
+        this.interests = interests;
+        this.why_ican_host = why_ican_host;
+        this.Countries_ivisited = Countries_ivisited
+    }
+  }
+  /*
+first name #1
+last name #1
+M/F #1 
+Hosting Availability  accepting/maybe/not accepting guests
+where i live now
+Occupation
+Education
+Languages I'm Fluent In #1
+About Me 
+Why I'm on Couchsurfing
+My Interests    (array of interests)
+What I Can Share With Hosts
+Countries I've Visited*/
+
+/*-my home:
+Available Nights to Host (check days)
+Maximum Number of Guests
+Preferred Gender to Host
+Children, Pets, Smoking (check boxes (are they allowed))
+Sleeping Arrangements (shared/private/public room)
+Description of Sleeping Arrangement
+Roommate Situation
+Miscellaneous (me (pets/have kids/smoking))
+Public Transportation Access (yes/no) (actually a text input)
+What I Can Share With Guests
+Additional Information
+food included?*/
